1241ea8fa5013debd541d3b5b2602a807c46b7a8
anhzin <nguyenanhzin@gmail.com>
2019-07-04 19:18:51 +0700

    update systemjs

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,4 +1,4 @@
-(function (global) {
+(function(global) {
     System.config({
         defaultJSExtensions: true,
         baseUrl: '.',
@@ -20,7 +20,7 @@
         },
         paths: {
             npm: "node_modules/"
-            //'npm:': 'https://unpkg.com/'
+                //'npm:': 'https://unpkg.com/'
         },
         map: {
             "src": "src",
@@ -35,11 +35,16 @@
             '@angular/upgrade': 'npm@angular/upgrade/bundles/upgrade.umd.js',
             '@angular/upgrade/static': 'npm@angular/upgrade/bundles/upgrade-static.umd.js',
             'rxjs': 'npmrxjs',
+            '@app/common': 'src/modules/common',
             /*'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',*/
             'ts': 'npm@plugin-typescript@4.0.10/lib/plugin.js',
             'typescript': 'npm@typescript@2.0.3/lib/typescript.js'
         },
         packages: {
+            "@app/common": {
+                main: "index",
+                defaultExtension: "js"
+            },
             src: {
                 main: "./main.ts",
                 defaultExtension: "js"
@@ -50,31 +55,33 @@
         }
     });
     if (global.autoBootstrap) { bootstrap(); }
+
     function bootstrap() {
         console.log("System was bootstrapped automaticly");
         //System.set(System.normalizeSync("src/main.ts"), System.newModule({}));
         Promise.all([
             System.import("@angular/platform-browser-dynamic"),
             getModule("defaultPage")
-        ]).then(function (imports) {
+        ]).then(function(imports) {
             var platform = imports[0];
             var module = imports[1];
             platform.platformBrowserDynamic().bootstrapModule(module.appModule)
-        }).catch(function (err) {
+        }).catch(function(err) {
             console.log("import modules with error:", err);
         });
     }
+
     function getModule(modulePath) {
         console.log("Load module", modulePath);
         return Promise.all([
             System.import("@angular/core"),
             System.import("@angular/platform-browser"),
             System.import("src/" + modulePath),
-        ]).then(function (imports) {
+        ]).then(function(imports) {
             var core = imports[0];
             var browser = imports[1];
             var module = imports[2].Module;
-            var appModule = function(){ };
+            var appModule = function() {};
             appModule.annotations = [
                 new core.NgModule({
                     imports: [browser.BrowserModule],
